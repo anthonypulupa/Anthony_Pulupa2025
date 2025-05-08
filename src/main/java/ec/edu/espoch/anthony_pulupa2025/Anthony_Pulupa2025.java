3479dac2f9c7ab6e3988d2fbb04736614875a42b
anthonypulupa <anthonypulupa2@gmail.com>
2025-05-08 17:12:41 -0500

    Leccion1

---
+++ src/main/java/ec/edu/espoch/anthony_pulupa2025/Anthony_Pulupa2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.anthony_pulupa2025;
+
+/**
+ *
+ * @author anthony
+ */
+public class Anthony_Pulupa2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
